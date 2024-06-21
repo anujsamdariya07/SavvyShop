c5c9cf5c0326f1869ddc08130ee27bd755679a94
anujsamdariya07 <anujsamdariya07@gmail.com>
2024-06-21 12:21:01 +0530

    Adding Category in Routes +

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,9 @@
 import { Footer, Navbar } from './components'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Cart, LoginSignUp, Product, Shop, ShopCategory } from './pages'
+import men_banner from './components/assets/banner_mens.png'
+import women_banner from './components/assets/banner_women.png'
+import kid_banner from './components/assets/banner_kids.png'
 
 function App() {
   return (
@@ -10,9 +13,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<Shop />} />
-          <Route path='/men' element={<ShopCategory category='men' />} />
-          <Route path='/women' element={<ShopCategory category='women' />} />
-          <Route path='/kids' element={<ShopCategory category='kids' />} />
+          <Route path='/men' element={<ShopCategory banner={men_banner} category='men' />} />
+          <Route path='/women' element={<ShopCategory banner={women_banner} category='women' />} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category='kids' />} />
           <Route path='product' element={<Product />} >
             <Route path=':productId' element={<Product />} />
           </Route>
